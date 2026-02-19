2c9e923e22d7c204b2e74076aeedb7ac32c3a45e
user-firepluto <eduardovianapires987@gmail.com>
2026-02-19 13:53:38 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,9 +2,7 @@
 chmod +x main.py
 chmod +x mass.py
 chmod +x unit-conversor
-chmod +x remove.sh
-sudo mkdir /home/$USER/.miniprojects/unit-conversor
-sudo cp unit-conversor /usr/local/bin
-sudo cp main.py /home/$USER/.miniprojects/unit-conversor/
-sudo cp mass.py /home/$USER/.miniprojects/unit-conversor/
-echo "installation completed"
+sudo mkdir /usr/local/bin/unit
+sudo cp unit-conversor /usr/local/bin/unit
+sudo cp main.py /usr/local/bin/unit
+sudo cp mass.py /usr/local/bin/unit
